22d431c14b9ba4d49461f9ec4d2fb82dc3530ea9
Redwanthedeveloper <redwan.thedeveloper@gmail.com>
2022-01-12 03:59:43 +0600

    team & slider changed

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import Slider from 'react-slick';
-import sliderImage1 from '../assets/images/slider/1.png';
-import sliderImage2 from '../assets/images/slider/2.png';
-import sliderImage4 from '../assets/images/slider/4.png';
-import sliderImage5 from '../assets/images/slider/5.png';
-import sliderImage6 from '../assets/images/slider/6.png';
-import sliderImage7 from '../assets/images/slider/7.png';
+import React from "react";
+import Slider from "react-slick";
+import sliderImage1 from "../assets/images/slider/1.png";
+import sliderImage2 from "../assets/images/slider/2.png";
+import sliderImage3 from "../assets/images/slider/3.png";
+import sliderImage4 from "../assets/images/slider/4.png";
+import sliderImage5 from "../assets/images/slider/5.png";
+import sliderImage6 from "../assets/images/slider/6.png";
+import sliderImage7 from "../assets/images/slider/7.png";
+import sliderImage8 from "../assets/images/slider/8.png";
+import sliderImage9 from "../assets/images/slider/9.png";
 
 const ImageSlider = () => {
   const settings = {
-    className: 'center',
+    className: "center",
     centerMode: true,
     infinite: true,
-    centerPadding: '60px',
+    centerPadding: "60px",
     slidesToShow: 5,
     speed: 1000,
     autoplay: true,
@@ -33,26 +36,34 @@ const ImageSlider = () => {
     <>
       <div>
         <Slider {...settings}>
-          <div className='mx-2'>
-            <img src={sliderImage1} alt='' className='max-w-full' />
+          <div className="mx-2">
+            <img src={sliderImage1} alt="" className="max-w-full" />
           </div>
 
-          <div className='mx-2'>
-            <img src={sliderImage2} alt='' className='max-w-full' />
+          <div className="mx-2">
+            <img src={sliderImage2} alt="" className="max-w-full" />
+          </div>
+          <div className="mx-2">
+            <img src={sliderImage3} alt="" className="max-w-full" />
+          </div>
+          <div className="mx-2">
+            <img src={sliderImage4} alt="" className="max-w-full" />
+          </div>
+          <div className="mx-2">
+            <img src={sliderImage5} alt="" className="max-w-full" />
+          </div>
+          <div className="mx-2">
+            <img src={sliderImage6} alt="" className="max-w-full" />
           </div>
 
-          <div className='mx-2'>
-            <img src={sliderImage4} alt='' className='max-w-full' />
+          <div className="mx-2">
+            <img src={sliderImage7} alt="" className="max-w-full" />
           </div>
-          <div className='mx-2'>
-            <img src={sliderImage5} alt='' className='max-w-full' />
+          <div className="mx-2">
+            <img src={sliderImage8} alt="" className="max-w-full" />
           </div>
-          <div className='mx-2'>
-            <img src={sliderImage6} alt='' className='max-w-full' />
-          </div>
-
-          <div className='mx-2'>
-            <img src={sliderImage7} alt='' className='max-w-full' />
+          <div className="mx-2">
+            <img src={sliderImage9} alt="" className="max-w-full" />
           </div>
         </Slider>
       </div>
--- src/components/Team.js
+++ src/components/Team.js
@@ -1,64 +1,65 @@
-import React from 'react';
-import image1 from '../assets/images/about.png';
-import image2 from '../assets/images/about.png';
-import image3 from '../assets/images/about.png';
+import React from "react";
+import image1 from "../assets/images/team/1.png";
+import image2 from "../assets/images/team/2.png";
+import image3 from "../assets/images/team/3.png";
+import image4 from "../assets/images/team/4.png";
 
 const Team = () => {
   return (
     <>
-      <div className='team lg:mt-24 py-12 bg-[#032452e3]' id='team'>
-        <div className='container mx-auto'>
-          <div className='team__inner px-6'>
-            <h1 className='font-bold text-2xl text-center pb-6 lg:pb-12 text-white lg:text-center lg:text-3xl uppercase '>
+      <div className="team lg:mt-24 py-12 bg-[#032452e3]" id="team">
+        <div className="container mx-auto">
+          <div className="team__inner px-6">
+            <h1 className="font-bold text-2xl text-center pb-6 lg:pb-12 text-white lg:text-center lg:text-3xl uppercase ">
               Our Team
             </h1>
-            <div className='team__wrapper grid grid-cols-1 gap-12 md:grid-cols-4 md:gap-8'>
+            <div className="team__wrapper grid grid-cols-1 gap-12 md:grid-cols-4 md:gap-8">
               <div
-                className='team__content  border-2 border-[#18345a] py-4 px-4 rounded-lg '
-                data-aos='fade-up'
-                data-aos-duration='1000'
+                className="team__content  border-2 border-[#18345a] py-4 px-4 rounded-lg "
+                data-aos="fade-up"
+                data-aos-duration="1000"
               >
-                <img src={image1} alt='' className='max-w-full   mx-auto' />
-                <h3 className='text-center text-lg lg:text-xl uppercase font-extrabold py-4 text-white'>
+                <img src={image1} alt="" className="max-w-full   mx-auto" />
+                <h3 className="text-center text-lg lg:text-xl uppercase font-extrabold py-4 text-white">
                   @jadenpatton1
                 </h3>
-                <p className='text-center text-white'>
+                <p className="text-center text-white">
                   Rompercrush - Developer
                 </p>
               </div>
               <div
-                className='team__content  border-2 border-[#18345a] py-4 px-4 rounded-lg '
-                data-aos='fade-up'
-                data-aos-duration='1200'
+                className="team__content  border-2 border-[#18345a] py-4 px-4 rounded-lg "
+                data-aos="fade-up"
+                data-aos-duration="1200"
               >
-                <img src={image2} alt='' className='max-w-full   mx-auto' />
-                <h3 className='text-center text-lg lg:text-xl uppercase font-extrabold py-4 text-white'>
+                <img src={image2} alt="" className="max-w-full   mx-auto" />
+                <h3 className="text-center text-lg lg:text-xl uppercase font-extrabold py-4 text-white">
                   @gmute_
                 </h3>
-                <p className='text-center text-white'>gmute - Community Lead</p>
+                <p className="text-center text-white">gmute - Community Lead</p>
               </div>
               <div
-                className='team__content  border-2 border-[#18345a] py-4 px-4 rounded-lg '
-                data-aos='fade-up'
-                data-aos-duration='1400'
+                className="team__content  border-2 border-[#18345a] py-4 px-4 rounded-lg "
+                data-aos="fade-up"
+                data-aos-duration="1400"
               >
-                <img src={image3} alt='' className='max-w-full   mx-auto' />
-                <h3 className='text-center text-lg lg:text-xl uppercase font-extrabold py-4 text-white'>
+                <img src={image3} alt="" className="max-w-full   mx-auto" />
+                <h3 className="text-center text-lg lg:text-xl uppercase font-extrabold py-4 text-white">
                   @jaarri
                 </h3>
-                <p className='text-center text-white'>Jarrii - Marketing</p>
+                <p className="text-center text-white">Jarrii - Marketing</p>
               </div>
 
               <div
-                className='team__content  border-2 border-[#18345a] py-4 px-4 rounded-lg '
-                data-aos='fade-up'
-                data-aos-duration='1400'
+                className="team__content  border-2 border-[#18345a] py-4 px-4 rounded-lg "
+                data-aos="fade-up"
+                data-aos-duration="1400"
               >
-                <img src={image3} alt='' className='max-w-full   mx-auto' />
-                <h3 className='text-center text-lg lg:text-xl uppercase font-extrabold py-4 text-white'>
+                <img src={image4} alt="" className="max-w-full   mx-auto" />
+                <h3 className="text-center text-lg lg:text-xl uppercase font-extrabold py-4 text-white">
                   @NFTCast
                 </h3>
-                <p className='text-center text-white'>NFTCasT - Artist</p>
+                <p className="text-center text-white">NFTCasT - Artist</p>
               </div>
             </div>
           </div>
